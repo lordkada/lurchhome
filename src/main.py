5233c8cbbdecef203ec2658963464973baef3aea
Carlo Alberto Degli Atti <lordkada@gmail.com>
2025-08-15 16:50:42 +0200

    Add ASCII banner and avoid pipenv's complains of running in a virtual env

--- src/main.py
+++ src/main.py
@@ -18,7 +18,21 @@ async def run():
         await connector_task
 
 if __name__ == "__main__":
-    print("Welcome to the LurchHome project")
+    print(r"""
+      L   U   U RRRR   CCCC  H  H  H  H  OOO   M   M  EEEE
+      L   U   U R   R C      H  H  H  H O   O  MM MM  E   
+      L   U   U RRRR  C      HHHH  HHHH O   O  M M M  EEE 
+      L   U   U R R   C      H  H  H  H O   O  M   M  E   
+      LLL  UUU  R  R   CCCC  H  H  H  H  OOO   M   M  EEEE
+
+    +------------------------------------------------------------------+
+    | Project : LurchHome                                              |
+    | License : Apache 2.0                                             |
+    | Repo    : https://github.com/lordkada/lurchhome                  |
+    +------------------------------------------------------------------+
+    | Smart Butler for Home Assistant • AI orchestration & automations |
+    +------------------------------------------------------------------+
+    """)
 
     parser = argparse.ArgumentParser(description="Set the logging level via command line")
     parser.add_argument('--log', default='WARNING',
